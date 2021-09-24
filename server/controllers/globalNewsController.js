0b463a45c61acddff8b9c1311079d6312247f42b
Denwa <denwa799@yandex.ru>
2021-09-24 14:35:13 +0300

    Add get all global news API

--- server/controllers/globalNewsController.js
+++ server/controllers/globalNewsController.js
@@ -7,23 +7,44 @@ const ApiError = require('../error/ApiError')
 class GlobalNewsController {
     async create(req, res, next) {
         try {
-            let {title, preview, text, sectionId} = req.body
+            let {title, preview, text, sectionId, regionId} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             await img.mv(path.resolve(__dirname, '..', 'static', fileName))
 
-            const news = await GlobalNews.create({title, preview, text, sectionId, img: fileName})
+            const news = await GlobalNews.create({title, preview, text, sectionId, regionId, img: fileName})
 
             return res.json(news)
-        }catch (e) {
+        } catch (e) {
             next(ApiError.badRequest((e.message)))
         }
 
     }
 
-    async getAll(req, res) {
+    async getAll(req, res, next) {
+        try {
+            const {sectionId, regionId} = req.body
+            let news
 
+            if (!sectionId && !regionId) {
+                news = await GlobalNews.findAll()
+            }
+            if (sectionId && !regionId) {
+                news = await GlobalNews.findAll({where: {sectionId}})
+            }
+            if (!sectionId && regionId) {
+                news = await GlobalNews.findAll({where: {regionId}})
+            }
+            if (sectionId && regionId) {
+                news = await GlobalNews.findAll({where: {sectionId, regionId}})
+            }
+
+            return res.json(news)
+        } catch (e) {
+            next(ApiError.badRequest((e.message)))
+        }
     }
+
     async getOne(req, res) {
 
     }
