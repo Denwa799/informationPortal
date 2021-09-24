01ba1f708b1342b1d738d14addbbcedef2971e82
Denwa <denwa799@yandex.ru>
2021-09-24 14:20:58 +0300

    Add try/catch to sections controller

--- server/controllers/sectionsController.js
+++ server/controllers/sectionsController.js
@@ -2,23 +2,37 @@ const {Sections} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class SectionsController {
-    async create(req, res) {
-        const {name} = req.body
-        const section = await Sections.create({name})
-        return res.json(section)
+    async create(req, res, next) {
+        try {
+            const {name} = req.body
+            const section = await Sections.create({name})
+            return res.json(section)
+        } catch (e) {
+            next(ApiError.badRequest((e.message)))
+        }
+
     }
 
-    async getAll(req, res) {
-        const sections = await Sections.findAll()
-        return res.json(sections)
+    async getAll(req, res, next) {
+        try {
+            const sections = await Sections.findAll()
+            return res.json(sections)
+        } catch (e) {
+            next(ApiError.badRequest((e.message)))
+        }
+
     }
 
-    async getOne(req, res) {
-        const {id} = req.params
-        const section = await Sections.findOne(
-            {where: {id}}
-        )
-        return res.json(section)
+    async getOne(req, res, next) {
+        try {
+            const {id} = req.params
+            const section = await Sections.findOne(
+                {where: {id}}
+            )
+            return res.json(section)
+        } catch (e) {
+            next(ApiError.badRequest((e.message)))
+        }
     }
 }
 
