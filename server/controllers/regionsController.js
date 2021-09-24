d454dd5a7e0bcc19c930ea523ab8cf826d2b03c7
Denwa <denwa799@yandex.ru>
2021-09-24 14:08:43 +0300

    Add try/catch to regions controller

--- server/controllers/regionsController.js
+++ server/controllers/regionsController.js
@@ -2,23 +2,35 @@ const {Regions} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class RegionsController {
-    async create(req, res) {
-        const {name} = req.body
-        const region = await Regions.create({name})
-        return res.json(region)
+    async create(req, res, next) {
+        try {
+            const {name} = req.body
+            const region = await Regions.create({name})
+            return res.json(region)
+        } catch (e) {
+            next(ApiError.badRequest((e.message)))
+        }
     }
 
-    async getAll(req, res) {
-        const regions = await Regions.findAll()
-        return res.json(regions)
+    async getAll(req, res, next) {
+        try {
+            const regions = await Regions.findAll()
+            return res.json(regions)
+        } catch (e) {
+            next(ApiError.badRequest((e.message)))
+        }
     }
 
-    async getOne(req, res) {
-        const {id} = req.params
-        const region = await Regions.findOne(
-            {where: {id}}
-        )
-        return res.json(region)
+    async getOne(req, res, next) {
+        try {
+            const {id} = req.params
+            const region = await Regions.findOne(
+                {where: {id}}
+            )
+            return res.json(region)
+        } catch (e) {
+            next(ApiError.badRequest((e.message)))
+        }
     }
 }
 
