455d6c37f913e99de2694b5e1fdd76c8693dcfa1
Denwa <denwa799@yandex.ru>
2021-09-26 15:55:35 +0300

    Add anime news API

--- server/routes/animeNewsRouter.js
+++ server/routes/animeNewsRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const router = new Router()
+const animeNewsController = require('../controllers/animeNewsController')
 
-router.post('/',)
-router.get('/',)
-router.get('/:id',)
+router.post('/', animeNewsController.create)
+router.get('/', animeNewsController.getAll)
+router.get('/:id', animeNewsController.getOne)
+router.delete('/:id', animeNewsController.delete)
 
 
 module.exports = router
